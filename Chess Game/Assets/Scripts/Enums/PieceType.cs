563e2ae6f70ab256b144186e567187176896176f
nikolajIvanov <62962459+nikolajIvanov@users.noreply.github.com>
2023-01-08 14:02:48 +0000

    Neue Skripte

---
+++ Chess Game/Assets/Scripts/Enums/PieceType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PieceType
+{
+    // enums =  special "class" that contains a set of named integer constants.
+    //          Use enums when you habe values that you know will not change,
+    //          To get the integer value from an item, you must explicitly convert to an int
+    
+    // Bauer, Läufer, Springer, Turn, Dame, König
+    Pwan, Bishop, Knight, Rook, Queen, King
+}
